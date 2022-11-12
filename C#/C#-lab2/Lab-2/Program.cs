1d4585fc96eb92ad1ee45105a47557bf65b4dea8
Mohammed Saad <59375560+msaad7777@users.noreply.github.com>
2022-11-11 22:33:44 -0500

    C#

---
+++ C#/C#-lab2/Lab-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int index = 1;
+            int depreciation = 4000;
+            int EOY_Value = 24000;
+            int accumulated_Depreciation = 4000;
+            bool toContinue = true;
+            Console.Write("  YEAR     DEPRECIATION       END-OF-YEAR VALUE     ACCUMULATED DEPRECIATION \n");
+            Console.Write("  ----     ------------       -----------------     ------------------------ \n");
+            do
+            {
+                
+                Console.Write($" {index,4}         {depreciation}           {EOY_Value,12}                    {accumulated_Depreciation}\n");
+                
+                if (EOY_Value > 0)
+                {
+                    index = index + 1;
+                    EOY_Value = EOY_Value - depreciation;
+                    accumulated_Depreciation = accumulated_Depreciation + depreciation;
+                }
+                else
+                {
+                    toContinue = false;
+                }
+
+            } while (toContinue);
+            
+        }
+    }
+}
